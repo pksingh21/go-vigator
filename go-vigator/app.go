06895a368f9b77938f285c76cc112f09c1a79d7f
Prateek Kumar Singh <20cs01003@iitbbs.ac.in>
2023-09-16 00:39:55 +0530

    added basic ui

--- go-vigator/app.go
+++ go-vigator/app.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/user"
+	"syscall"
 )
 
 // App struct
@@ -22,6 +25,66 @@ func (a *App) startup(ctx context.Context) {
 }
 
 // Greet returns a greeting for the given name
+
+type File struct {
+	isDirectory bool
+	isFile      bool
+	Name        string
+	Owner       string
+	Group       string
+	// add a time field for last modified
+	// add a size field for size of file
+	latestTime string
+	size       string
+}
+
+func convertiKBorMB(size int64) string {
+	// function to convert the given 64 byte size to KB or MB and add KB or MB to the end of the size
+	// if size is less than 1024 then return size + KB
+	// if size is greater than 1024 then return size / 1024 + MB
+	if size >= 1024*1024 {
+		return fmt.Sprint(size/(1024*1024)) + "MB"
+	} else if size >= 1024 {
+		return fmt.Sprint(size/1024) + "KB"
+	} else {
+		return fmt.Sprint(size) + "B"
+	}
+}
+func GetFilesAndDirectories(path string) []File {
+	var filesAndDirectories []File
+	fileInfos, err := os.ReadDir(path)
+	if err != nil {
+		return nil
+	}
+
+	for _, fileInfo := range fileInfos {
+		if fileInfo.Name() == "." || fileInfo.Name() == ".." {
+			continue
+		}
+		extraFileInfo, _ := os.Stat(path + "/" + fileInfo.Name())
+		uuid := extraFileInfo.Sys().(*syscall.Stat_t).Uid
+		uuid_string := fmt.Sprint(uuid)
+		userName, _ := user.LookupId(uuid_string)
+		groupName, _ := user.LookupGroupId(fmt.Sprint(extraFileInfo.Sys().(*syscall.Stat_t).Gid))
+		file := File{
+			isDirectory: fileInfo.IsDir(),
+			isFile:      !fileInfo.IsDir(),
+			Name:        fileInfo.Name(),
+			Owner:       userName.Name,
+			Group:       groupName.Name,
+			latestTime:  extraFileInfo.ModTime().String(),
+			size:        convertiKBorMB(extraFileInfo.Size()),
+		}
+		filesAndDirectories = append(filesAndDirectories, file)
+	}
+
+	return filesAndDirectories
+}
+
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) GetFiles(path string) []File {
+	return GetFilesAndDirectories(path)
 }
--- go-vigator/frontend/src/App.tsx
+++ go-vigator/frontend/src/App.tsx
@@ -1,28 +1,57 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
-
+import { useState } from "react";
+import logo from "./assets/images/logo-universal.png";
+import "./App.css";
+import { Greet } from "../wailsjs/go/main/App";
+import { GetFiles } from "../wailsjs/go/main/App";
+interface File {
+  isDirectory: boolean;
+  isFile: boolean;
+  Name: string;
+  Owner: string;
+  Group: string;
+  latestTime: string;
+  size: string;
+}
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇"
+  );
+  const [name, setName] = useState("");
+  const [files, setFiles] = useState<File[]>([]);
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
+  const updateFiles = (result: File[]) => setFiles(result);
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
+  function getFileInfo() {
+    GetFiles(name).then(updateFiles);
+    console.log(files);
+  }
+  return (
+    <div id="App">
+      <img src={logo} id="logo" alt="logo" />
+      <div id="result" className="result">
+        {resultText}
+      </div>
+      <div id="input" className="input-box">
+        <input
+          id="name"
+          className="input"
+          onChange={updateName}
+          autoComplete="off"
+          name="input"
+          type="text"
+        />
+        <button className="btn" onClick={getFileInfo}>
+          Greet
+        </button>
+        {files.map((file) => (
+          <div>{file.Name}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
