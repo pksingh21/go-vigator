bf602977fef12ea1c1510ebb744415ccda1dbd33
pksingh21 <20cs01003@iitbbs.ac.in>
2023-09-29 18:05:27 +0530

    added some metrics

--- go-vigator/search_qeury.go
+++ go-vigator/search_qeury.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
+	"time"
 
 	"github.com/lithammer/fuzzysearch/fuzzy"
 	"github.com/pksingh21/go-vigator/filesystemsearch"
@@ -15,21 +16,26 @@ import (
 
 func ExecuteSearchQuery(query string, path string) (fuzzy.Ranks, error) {
 	// open the treeNew1.bin.gz file
+
+	start := time.Now()
 	var rootFolder *filesystemsearch.Folder
 	file, err := os.Open("treeNew1.bin.gz")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		rootFolde := filesystemsearch.BuildTree()
-		fmt.Println("building tree done")
+		elapsed := time.Since(start)
+		fmt.Println("building tree done in ", elapsed.Abs().Minutes(), " minutes")
 		rootFolder = rootFolde
 	}
 	defer file.Close()
+	start = time.Now()
 	file, _ = os.Open("treeNew1.bin.gz")
 	gzipReader, err := gzip.NewReader(file)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return fuzzy.Ranks{}, err
 	}
+	elapsed := time.Since(start)
 	defer gzipReader.Close()
 	decoder := gob.NewDecoder(gzipReader)
 	err = decoder.Decode(&rootFolder)
@@ -37,9 +43,8 @@ func ExecuteSearchQuery(query string, path string) (fuzzy.Ranks, error) {
 		fmt.Println("Error decoding binary data")
 		return fuzzy.Ranks{}, err
 	}
-
+	fmt.Println("Decoded tree in ", elapsed.Abs().Seconds(), " seconds")
 	segments := strings.Split(path, string(filepath.Separator))
-	fmt.Println(segments)
 
 	for i, segment := range segments {
 		if i >= 1 && len(segment) > 0 {
@@ -47,20 +52,21 @@ func ExecuteSearchQuery(query string, path string) (fuzzy.Ranks, error) {
 			rootFolder = rootFolder.Folders[segment]
 		}
 	}
-	// rootFolder = rootFolder.Folders["Users"].Folders["Ankit"].Folders["Downloads"]
-	// fmt.Println(rootFolder.Files)
-	// rootFolder = rootFolder.Folders["Users"].Folders["Ankit"].Folders["Downloads"]
-	// fmt.Println(rootFolder.Folders)
-
 	go filesystemsearch.Watch(rootFolder)
 	filesystemsearch.Path = []string{}
 	rootFolder.String("", 0)
+	start = time.Now()
 	wordx := fuzzy.RankFindFold(query, filesystemsearch.Path)
+	elapsed = time.Since(start)
 	fmt.Println("Path Array built with length ", len(filesystemsearch.Path))
 	sort.Sort(wordx)
+	fmt.Println("Search results generated in ", elapsed.Abs().Seconds(), " seconds")
 	if len(wordx) > 200 {
 		wordx = wordx[:200]
 	}
+	start = time.Now()
 	filesystemsearch.Encode(rootFolder)
+	elapsed = time.Since(start)
+	fmt.Println("Encoded tree in ", elapsed.Abs().Seconds(), " seconds")
 	return wordx, nil
 }
